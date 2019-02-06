358b8a7f3c4322f442d1e74a8dbf5943f205864f
mackosi1@student.pg.gda.pl <kotek123>
2019-02-06 18:45:17 +0100

    res update

--- bee_colony/utilities.py
+++ bee_colony/utilities.py
@@ -2,13 +2,19 @@
 
 
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
 
 
 def plot(data, func_name):
-    font = FontProperties()
-    font.set_size('larger')
-    plt.figure(1, figsize=(21,14))
+    SMALL_SIZE = 16
+
+
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+    plt.figure(1, figsize=(21, 14))
 
     plt.subplot(221)
     plt.plot(data["all_population_func_x"], data["all_population_func_y"], 'ro', markersize=1)
